72152776b57fa7d782aeb91a5089124f8d889be6
Nicolas <cuellern@gmail.com>
2023-01-05 21:26:40 +0900

    complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+let i = null
+function countDown(i){
+    while(i >= 0){
+        console.log(i)
+        i--
+    }
+}
+
+function writeCards(names, event){
+    let message = []
+    for(i = 0; i < names.length; i++){
+        message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return message
+}
